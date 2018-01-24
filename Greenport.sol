4854b73a511ee7c43d53baaf9ca1627144309118
ThunderDeliverer <jan.turk12@gmail.com>
2018-01-24 18:24:24 +0100

    Added user inspection function.

--- Greenport.sol
+++ Greenport.sol
@@ -122,6 +122,11 @@ contract Greenport is controlled{
     return balances[_tokenOwner];
   }
 
+  function viewUser(uint256 _userID) constant public returns (int256 theNegativeValues, int256 thePositiveValues, int256 theLastNegativeValues, int256 theLastPositiveValues, int256 theCurrentDelta, int256 theAverageDelta, int256 theMonthCounter){
+    var theUser = users[_userID];
+    return(theUser.negativeValues, theUser.positiveValues, theUser.lastNegativeValues, theUser.lastPositiveValues, theUser.currentDelta, theUser.averageDelta, theUser.monthCounter);
+  }
+
   /*
   * @dev Lets users share tokens and will allow us to implement loyality programs.
   * @param _userID uint256 User that sends the credits.
