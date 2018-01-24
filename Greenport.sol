2b997332598750ad4253ff88f0e05f1011274837
ThunderDeliverer <jan.turk12@gmail.com>
2018-01-24 16:39:24 +0100

    Updated comments to adhere to the protocol.

--- Greenport.sol
+++ Greenport.sol
@@ -4,6 +4,9 @@ contract controlled{
   address owner;
   mapping (address => bool) isAdministrator;
 
+  /*
+  * @dev Deply function for controll mechanics.
+  */
   function controlled() public{
     owner = msg.sender;
   }
@@ -71,10 +74,10 @@ contract Greenport is controlled{
   mapping (uint256 => bool) isRegistered;
 
   /*
-  * @dev Informs anyone listening that transaction of tokens took place.
-  * @param from address Address that sends the tokens.
-  * @param to address Address that receives the tokens.
-  * @param value uint256 Amount of tokens transferred.
+  * @dev Informs anyone listening that transaction of credits took place.
+  * @param from uint256 User that sends the credits.
+  * @param to uint256 User that receives the credits.
+  * @param value int256 Amount of credits transferred.
   */
   event Transfer(uint256 indexed from, uint256 indexed to, int256 value);
 
@@ -83,9 +86,8 @@ contract Greenport is controlled{
   * @param _name string Sets full, possibly multiword, name of the token.
   * @param _symbol string Sets abbriveration or symbol of our token.
   * @param _decimals uint8 Sets number of decimal places used by our token.
-  * @param _startingSupply uint256 Creates specified amount of tokens at deployement to contract address-
+  * @param _startingSupply uint256 Creates specified amount of tokens at deployement to contract address.
   * @param _initialOwner address Apponts owner of the contract. If left blank, the contract will set the deployer as owner.
-  * @param _initialEtherRetrievalDestination  address Sets the address contract sends Ether to. If left blank, contract wil set it to deployers address.
   */
   function Greenport(string _name, string _symbol, uint8 _decimals, int256 _startingSupply, address _initialOwner) public{
     name = _name;
@@ -114,13 +116,18 @@ contract Greenport is controlled{
 
   /*
   * @dev Allows us to view the token balance of the account.
-  * @param _tokenOwner address Address of the user whose token balance we are trying to view.
+  * @param _tokenOwner uint256 Address of the user whose credits balance we are trying to view.
   */
   function balanceOf(uint256 _tokenOwner) constant public returns (int256 accountBalance){
     return balances[_tokenOwner];
   }
 
-  //@dev Allows users to send tokens to an account.
+  /*
+  * @dev Lets users share tokens and will allow us to implement loyality programs.
+  * @param _userID uint256 User that sends the credits.
+  * @param _to uint256 User that receives the credist.
+  * @param _value int256 Amount of credits transferred.
+  */
   function transfer(uint256 _to, int256 _value, uint256 _userID) public returns (bool success){
     require(isRegistered[_to]);
     require(balances[_userID] >= _value);
@@ -160,6 +167,9 @@ contract Greenport is controlled{
     Transfer(0, _userID, mintedAmount);
   }
 
+  /*
+  * @dev Registers new user by their unique user ID.
+  */
   function registerNewUser () onlyAdministartor public returns(uint256 userIdentifier){
     numberOfUsers += 1;
     uint256 _userID = numberOfUsers;
@@ -176,18 +186,31 @@ contract Greenport is controlled{
     return(_userID);
   }
 
+  /*
+  * @dev Tracks negative actions of the user during the course of a month.
+  * @param _userID uint256  User ID of the user that commited actions with negaitve impact.
+  * @param _valuesToAdd int256 Variable to track the impact of user's negative actions.
+  */
   function negativeValues (uint256 _userID, int256 _valuesToAdd) onlyAdministartor public{
     var theUser = users[_userID];
     theUser.negativeValues = _valuesToAdd;
     theUser.currentDelta = theUser.positiveValues - theUser.negativeValues;
   }
 
+  /*
+  * @dev Tracks the impact of the users positive actions.
+  * @param _userID uint256 User ID of te user tat commited actions with positive impact.
+  * @param _valuesToAdd int256 VAriable to track the impact of user's positive actions.
+  */
   function positiveValues (uint256 _userID, int256 _valuesToAdd) onlyAdministartor public{
     var theUser = users[_userID];
     theUser.positiveValues = _valuesToAdd;
     theUser.currentDelta = theUser.positiveValues - theUser.negativeValues;
   }
 
+  /*
+  * @dev Assigns credits based on the current delta of positive vs. negative actions compared to current average for all users.
+  */
   function assignCredits () onlyAdministartor public{
     for(uint256 i=1; i <= numberOfUsers; i++){
       var theUser = users[i];
@@ -203,10 +226,18 @@ contract Greenport is controlled{
     }
   }
 
+  /*
+  * @dev Sets credit awarding factor that multiplies with the positive impacto of the user.
+  * @param _factor int256 The factor that awards users for positive behaviour (can be positive or negative, based on use case).
+  */
   function setDeltaCreditAwardFactor (int256 _factor) onlyAdministartor public{
     deltaCreditAwardFactor = _factor;
   }
 
+  /*
+  * @dev Determines how many credits a new user is awarded in the beginning. Used to make sure the first action of the user doesn't award them less than 0 credits.
+  * @param _amout int256 Amount of credit user receiv es at the beginning.
+  */
   function setRegistrationCreditsAmount (int256 _amount) onlyAdministartor public{
     registrationCredits = _amount;
   }
